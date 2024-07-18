3d65654e6fed04b3778111ae31f15f24f79cf943
david-fernandes-da-costa <david.dacosta@bm.pr.gov.br>
2024-07-18 20:46:05 -0300

    Edição do AutocompleteEditText

--- app/src/main/java/com/example/myipoapp/main/view/MainActivity.kt
+++ app/src/main/java/com/example/myipoapp/main/view/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.example.myipoapp
+package com.example.myipoapp.main.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.myipoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
