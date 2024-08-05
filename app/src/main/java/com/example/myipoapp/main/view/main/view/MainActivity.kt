78a99044a9cd381e49a96c921ccdccad226e35bf
David Fernandes da Costa <david.dacosta@gmail.com>
2024-08-05 10:13:51 -0300

    Criação do TextWatcher

--- app/src/main/java/com/example/myipoapp/main/view/main/view/MainActivity.kt
+++ app/src/main/java/com/example/myipoapp/main/view/main/view/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.example.myipoapp.main.view.main.view
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myipoapp.R
+import com.example.myipoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.mainBottomNav.setOnNavigationItemSelectedListener {item ->
+            when (item.itemId) {
+                R.id.menu_bottom_home -> {
+
+                    true
+                }
+                R.id.menu_bottom_userAdd-> {
+
+                    true
+                }
+                R.id.menu_bottom_userDel -> {
+
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myipoapp/main/view/register/view/HomeFragment.kt
+++ app/src/main/java/com/example/myipoapp/main/view/register/view/HomeFragment.kt
@@ -1,18 +1,42 @@
 package com.example.myipoapp.main.view.register.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.myipoapp.R
+import com.example.myipoapp.databinding.FragmentHomeBinding
+import com.example.myipoapp.main.view.common.util.TxtWatcher
 
-class HomeFragment: Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentHomeBinding.bind(view)
+
+        val listName = resources.getStringArray(R.array.testeName)
+        val adapterName = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, listName)
+        binding.selectNameComplete.setAdapter(adapterName)
+
+        binding.selectNameComplete.addTextChangedListener (watcher)
+
+        binding.selectStartButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Partiu criar formulário!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private val watcher = TxtWatcher{
+        binding.selectStartButton.isEnabled = binding.selectNameComplete.text.toString().isNotEmpty()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
