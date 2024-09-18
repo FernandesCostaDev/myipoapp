44a509968dffa1afb6c1a7355bdf8eecfc558a03
David Fernandes da Costa <david.dacosta@gmail.com>
2024-09-18 14:40:54 -0300

    Criação do fragment add user

--- app/src/main/java/com/example/myipoapp/main/view/register/view/HomeFragment.kt
+++ app/src/main/java/com/example/myipoapp/main/view/register/view/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.myipoapp.R
 import com.example.myipoapp.databinding.FragmentHomeBinding
@@ -12,31 +11,31 @@ import com.example.myipoapp.main.view.common.util.TxtWatcher
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+    private var binding: FragmentHomeBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentHomeBinding.bind(view)
+        binding = FragmentHomeBinding.bind(view)
 
-        val listName = resources.getStringArray(R.array.testeName)
-        val adapterName = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, listName)
-        binding.selectNameComplete.setAdapter(adapterName)
+       with(binding!!) {
+           val listName = resources.getStringArray(R.array.testeName)
+           val adapterName = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, listName)
+           selectNameComplete.setAdapter(adapterName)
 
-        binding.selectNameComplete.addTextChangedListener (watcher)
+          selectNameComplete.addTextChangedListener (watcher)
 
-        binding.selectStartButton.setOnClickListener {
-            Toast.makeText(requireContext(), "Partiu criar formulário!", Toast.LENGTH_SHORT).show()
-        }
-
+           selectStartButton.setOnClickListener {
+               Toast.makeText(requireContext(), "Partiu criar formulário!", Toast.LENGTH_SHORT).show()
+           }
+       }
     }
 
     private val watcher = TxtWatcher{
-        binding.selectStartButton.isEnabled = binding.selectNameComplete.text.toString().isNotEmpty()
+        binding?.selectStartButton?.isEnabled = binding?.selectNameComplete?.text.toString().isNotEmpty()
     }
 
     override fun onDestroyView() {
-        _binding = null
+        binding = null
         super.onDestroyView()
     }
 }
