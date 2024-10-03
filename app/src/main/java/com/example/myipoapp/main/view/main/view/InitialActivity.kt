2c1a52095d7f0f7260d9c51e6b9a4379b5dba247
David Fernandes da Costa <david.dacosta@gmail.com>
2024-10-03 11:53:29 -0300

    Implementação do layout da activity inicial

--- app/src/main/java/com/example/myipoapp/main/view/main/view/InitialActivity.kt
+++ app/src/main/java/com/example/myipoapp/main/view/main/view/InitialActivity.kt
@@ -1,16 +1,28 @@
 package com.example.myipoapp.main.view.main.view
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myipoapp.R
+import com.example.myipoapp.databinding.ActivityInitialBinding
+import com.example.myipoapp.databinding.ActivityMainBinding
 
 class InitialActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInitialBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initial)
+        binding = ActivityInitialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = binding.initialToolbar
+        setSupportActionBar(toolbar)
+        toolbar.overflowIcon?.setTint(Color.WHITE)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.brasao_bombeiro_p)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     }
 }
